a76dbc4e49e9a7222da5f7e4469477023e9f81f7
unknown <programacion@LABC29-03.fkinal.edu>
2018-08-22 15:53:04 -0600

    Migracion de datos a las tres tablas

--- LeerExcel/src/leerexcel/Principal.java
+++ LeerExcel/src/leerexcel/Principal.java
@@ -28,11 +28,9 @@ public class Principal {
      */
     public static void main(String[] args) throws Exception  {
 
+        String fileName = "C:\\Users\\informatica\\Desktop\\movies.xls";
 
-        String fileName = "C:\\Users\\informatica\\Desktop\\movies.xls";     
-        
-        
-            try {
+        try {
             ConnectionDB conn = new ConnectionDB();
             conn.connDataBase();
             InputStream myFile = new FileInputStream(new File(fileName));
@@ -49,6 +47,7 @@ public class Principal {
             for (int i = 0; i < sheet.getLastRowNum() + 1; i++) {
                 row = sheet.getRow(i);
                 String cadena_texto =  "";
+                
                 for (int j = 0; j < row.getLastCellNum(); j++) {
                     cell = row.getCell(j);
                     cadena_texto += cell.toString();
@@ -57,6 +56,7 @@ public class Principal {
                 String[] parts = cadena_texto.split("::");
                 String[] descandenarYear = parts[1].split("\\(");
                 String[] arrayCategory = parts[2].split("\\|");
+                
                 
                 if(descandenarYear.length > 3){
                     descandenarYear[0] += ("(" + descandenarYear[1] + "(" + descandenarYear[2]);
@@ -68,7 +68,6 @@ public class Principal {
                 }else{
                     year = descandenarYear[1].split("\\)")[0];
                 }
-                
                 for (int j = 0; j < arrayCategory.length; j++) {
                     if (arrayCat.isEmpty()) {
                         arrayCat.add(arrayCategory[j]);
@@ -80,16 +79,24 @@ public class Principal {
                         }
                     }
                 }
+             
+                System.out.println("id: "+parts[0] + " Movie: " + descandenarYear[0] + " Year: " + year); 
+                conn.enviarDatos("INSERT INTO pelicula (id, descripcion, year) values ('" + parts[0] + "', \"" + descandenarYear[0]  + "\", '" + year + "')");
                 
-                //System.out.println("id: "+parts[0] + " Movie: " + descandenarYear[0] + " Year: " + year + " Categoria: "+ parts[2]);
-                
-                //conn.enviarDatos("INSERT INTO pelicula (id, descripcion, year) values ('" + parts[0] + "', \"" + descandenarYear[0]  + "\", '" + year + "')");
+                for (int j = 0; j < arrayCat.size(); j++) {
+                    for (int k = 0;  k < arrayCategory.length; k++) {
+                        if(arrayCat.get(j).contains(arrayCategory[k])){
+                            conn.enviarDatos("INSERT INTO peliculascategorias (idPelicula, idCategoria) VALUES('" + parts[0] + "', '" + (j + 1) + "')");
+                        }
+                    }
+                }
             }
             System.out.println("!!!!!Finalizado!!!!!");
-                for (int i = 0; i < arrayCat.size(); i++) {
-                    //conn.enviarDatos("INSERT INTO categoria(descripcion) VALUES(\""+arrayCat.get(i)+"\")");
-                    System.out.println("Category: "+ arrayCat.get(i));
+                for (int l = 0; l < arrayCat.size(); l++) {
+                    conn.enviarDatos("INSERT INTO categoria(descripcion) VALUES(\""+arrayCat.get(l)+"\")");
+                    System.out.println("Category: "+ arrayCat.get(l));
                 }
+ 
         } catch (Exception e) {
             // TODO: handle exception
             System.out.println(e.getMessage());
